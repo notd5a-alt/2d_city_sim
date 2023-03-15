e943babeeb8746451615f117530ff95f607b5982
Ahmad Abu Shawer <87091319+notd5a-alt@users.noreply.github.com>
2023-03-15 17:38:30 +0400

    Delete build directory

--- build/CMakeFiles/MAIN.dir/include/building.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MAIN.dir/include/building.o: \
- /home/notd5a/dev/2d_city_sim/include/building.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/MAIN.dir/include/person.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MAIN.dir/include/person.o: \
- /home/notd5a/dev/2d_city_sim/include/person.cpp \
- /usr/include/stdc-predef.h
